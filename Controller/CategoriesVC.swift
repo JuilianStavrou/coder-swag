604d47af36e8271fa49252523d5524979b13e45b
Juilian Stavrou <juilian.stavrou@bigpond.com>
2018-04-16 08:42:16 +1000

    Lesson 1

--- Controller/CategoriesVC.swift
+++ Controller/CategoriesVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 //
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categotyTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ View/CategoryCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCellTableViewCell.swift
+//  coder-swag
+//
+//  Created by Code on 13/4/18.
+//  Copyright © 2018 Code. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
