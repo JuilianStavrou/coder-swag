67f9ee22fbf54d654f5c04613b2ea80d6cec7df1
Juilian Stavrou <juilian.stavrou@bigpond.com>
2018-04-16 11:18:34 +1000

    Lesson 3

---
+++ View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Code on 16/4/18.
+//  Copyright © 2018 Code. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
+
